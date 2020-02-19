4b2b02394815f852b2434a84253e36be6c0127a7
larnp <61011755+larnp@users.noreply.github.com>
2020-02-19 14:11:25 +0200

    Add files via upload

---
+++ first.table.sql
@@ -0,0 +1,25 @@
+/*
+-- Query: SELECT * FROM mydatabase.box
+LIMIT 0, 1000
+
+-- Date: 2020-02-19 14:09
+*/
+INSERT INTO `` (`Chips,,`,`stock`,`selling price`,`Chocolate`,`price`,`remaining stock`,`barcode`) VALUES ('nik naks','8','R1.50','Mint','R12.00','8',1);
+INSERT INTO `` (`Chips,,`,`stock`,`selling price`,`Chocolate`,`price`,`remaining stock`,`barcode`) VALUES ('simba','9','R4.50','Caramel','R12.00','12',2);
+INSERT INTO `` (`Chips,,`,`stock`,`selling price`,`Chocolate`,`price`,`remaining stock`,`barcode`) VALUES ('lays','7','R5.50','Vanilla','R12.00','5',3);
+INSERT INTO `` (`Chips,,`,`stock`,`selling price`,`Chocolate`,`price`,`remaining stock`,`barcode`) VALUES ('fritos','4','R400','nuts','R12.00','9',4);
+INSERT INTO `` (`Chips,,`,`stock`,`selling price`,`Chocolate`,`price`,`remaining stock`,`barcode`) VALUES ('Pies','Stock','Selling price','Cupcakes','Sellig Prices','Stock',5);
+INSERT INTO `` (`Chips,,`,`stock`,`selling price`,`Chocolate`,`price`,`remaining stock`,`barcode`) VALUES ('beef','3','R15.00','plain','R5.00','8',6);
+INSERT INTO `` (`Chips,,`,`stock`,`selling price`,`Chocolate`,`price`,`remaining stock`,`barcode`) VALUES ('cheese','11','R15.00','vanilla','R5.00','13',7);
+INSERT INTO `` (`Chips,,`,`stock`,`selling price`,`Chocolate`,`price`,`remaining stock`,`barcode`) VALUES ('steak','3','R15.00','choc','R5.00','16',8);
+INSERT INTO `` (`Chips,,`,`stock`,`selling price`,`Chocolate`,`price`,`remaining stock`,`barcode`) VALUES ('chicken ','','R15.00','Red Velvet','R10.00','',9);
+INSERT INTO `` (`Chips,,`,`stock`,`selling price`,`Chocolate`,`price`,`remaining stock`,`barcode`) VALUES ('Pies','Stock','Selling price','Cupcakes','Sellig Prices','Stock',10);
+INSERT INTO `` (`Chips,,`,`stock`,`selling price`,`Chocolate`,`price`,`remaining stock`,`barcode`) VALUES ('beef','3','R15.00','plain','R5.00','8',11);
+INSERT INTO `` (`Chips,,`,`stock`,`selling price`,`Chocolate`,`price`,`remaining stock`,`barcode`) VALUES ('cheese','11','R15.00','vanilla','R5.00','13',12);
+INSERT INTO `` (`Chips,,`,`stock`,`selling price`,`Chocolate`,`price`,`remaining stock`,`barcode`) VALUES ('steak','3','R15.00','choc','R5.00','16',13);
+INSERT INTO `` (`Chips,,`,`stock`,`selling price`,`Chocolate`,`price`,`remaining stock`,`barcode`) VALUES ('chicken ','','R15.00','Red Velvet','R10.00','',14);
+INSERT INTO `` (`Chips,,`,`stock`,`selling price`,`Chocolate`,`price`,`remaining stock`,`barcode`) VALUES ('Fruit','Stock','Selling Prices','Veggies','Selling Price','Stock',15);
+INSERT INTO `` (`Chips,,`,`stock`,`selling price`,`Chocolate`,`price`,`remaining stock`,`barcode`) VALUES ('apple','6','R3.00','onion','R2.50','17',16);
+INSERT INTO `` (`Chips,,`,`stock`,`selling price`,`Chocolate`,`price`,`remaining stock`,`barcode`) VALUES ('banana','8','R3.00','tomato','R2.50','14',17);
+INSERT INTO `` (`Chips,,`,`stock`,`selling price`,`Chocolate`,`price`,`remaining stock`,`barcode`) VALUES ('orange','12','R2.00','cabbage','R11.50','15',18);
+INSERT INTO `` (`Chips,,`,`stock`,`selling price`,`Chocolate`,`price`,`remaining stock`,`barcode`) VALUES ('peach','9','R2.00','betroot','R6.00','9',19);
