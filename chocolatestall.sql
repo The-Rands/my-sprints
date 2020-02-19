f6a54ba53d07342cce496ab0732b85d0a6b06f10
larnp <61011755+larnp@users.noreply.github.com>
2020-02-19 11:30:34 +0200

    Add files via upload

---
+++ chocolatestall.sql
@@ -0,0 +1,9 @@
+Type,Quantity,Profit,Size,Barcode,id
+"Dairy milk",56,R576,120g,00001,1
+KitKat,77,R1386,170g,00002,2
+londor,67,R936,21.9g,00003,3
+"Bar one",87,R786,140g,00003,4
+Snickers,55,R667,62g,00005,5
+Mars,44,R577,70g,00006,6
+NULL,,NULL,NULL,,
+NULL,NULL,NULL,NULL,,
