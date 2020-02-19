51e67a2744bfcc34be4148103d8a37be1e9b5459
larnp <61011755+larnp@users.noreply.github.com>
2020-02-19 11:16:19 +0200

    Add files via upload

---
+++ cupcake.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM mydatabase.cupcake
+LIMIT 0, 1000
+
+-- Date: 2020-02-19 11:14
+*/
+INSERT INTO `` (`Price`,`Quantity`,`Profit`,`Size`,`Barcode`,`Type`) VALUES ('R30','56','R750','Each','221','honeycomb');
+INSERT INTO `` (`Price`,`Quantity`,`Profit`,`Size`,`Barcode`,`Type`) VALUES ('R25','89','R506','Each','222','Raspberry');
+INSERT INTO `` (`Price`,`Quantity`,`Profit`,`Size`,`Barcode`,`Type`) VALUES ('R20','54','R860','Each','223','Red velvet');
+INSERT INTO `` (`Price`,`Quantity`,`Profit`,`Size`,`Barcode`,`Type`) VALUES ('R15','45','R456','Each','225','Strawberry');
+INSERT INTO `` (`Price`,`Quantity`,`Profit`,`Size`,`Barcode`,`Type`) VALUES ('R19','65','R798','Each','224','Vanilla');
