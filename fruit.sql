cc32747d07530cb576248d3ceaf46e347e7c4e0a
larnp <61011755+larnp@users.noreply.github.com>
2020-02-19 11:53:11 +0200

    Add files via upload

---
+++ fruit.sql
@@ -0,0 +1,8 @@
+Price,Quantity,Profit,Size,Barcode,Type
+R12,89,R2244,"per Kg",889,Banana
+R12,45,R2500,"per Kg",890,"Apple "
+R12,56,R3450,"per Kg",891,Mango
+R19,77,R1500,"per Kg",892,Grape
+R24,32,R880,"per Kg",893,Peach
+R25,66,R970,"per 2",894,Pinapples
+NULL,NULL,NULL,NULL,,NULL
